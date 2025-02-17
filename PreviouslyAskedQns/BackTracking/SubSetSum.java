c24ed3216249b53e25e33be37a12bbd1a75f579a
Barath <barathbarth128@gmail.com>
2025-02-17 14:13:49 +0530

    Daily-Dose

---
+++ PreviouslyAskedQns/BackTracking/SubSetSum.java
@@ -0,0 +1,17 @@
+package com.Barath.AdvancedDS.Recursion;
+
+public class SubSetSum {
+    public static void main(String[] args) {
+        int[] arr = {10,20,15,5};
+        System.out.println(countSubSets(arr,25,0));
+    }
+    static int countSubSets(int[] arr,int target,int i) {
+        if (target == 0) {
+            return 1;
+        }
+        if (target < 0 || i == arr.length) {
+            return 0;
+        }
+        return countSubSets(arr,target - arr[i],i + 1) + countSubSets(arr,target,i + 1);
+    }
+}
